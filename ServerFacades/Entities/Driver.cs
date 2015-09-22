f4c9f8db963972ecd9e14c83d0e22e49b285d605
Pat Howell <patrick.howell@outlook.com>
2015-09-22 21:57:40 +0100

    Started adding entites for code first db

---
+++ ServerFacades/Entities/Driver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerFacades.Entities
+{
+    public class Driver
+    {
+        public int DriverId { get; set; }
+
+        public string Name { get; set; }
+
+        public int RacesEntered { get; set; }
+
+        public int NumberOfPodiums { get; set; }
+
+        public int NUmberOfRaces { get; set; }
+    }
+}
---
+++ ServerFacades/Entities/Team.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerFacades.Entities
+{
+    public class TeamInChampionship
+    {
+        public Guid TeamID { get; set; }
+
+        public String TeamName { get; set; }
+    }
+}
