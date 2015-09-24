ef9ca9b165ffd67f8d211c5da62ebba4b77b9a39
Pat Howell <patrick.howell@outlook.com>
2015-09-24 21:27:50 +0100

    Removed un-used usings

--- ServerFacades/Entities/Driver.cs
+++ ServerFacades/Entities/Driver.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ServerFacades.Entities
+﻿namespace ServerFacades.Entities
 {
     public class Driver
     {
