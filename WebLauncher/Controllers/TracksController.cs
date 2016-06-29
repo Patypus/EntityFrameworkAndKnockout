8e1d816dd2ad97b0b827482b801ab7b891f4a38c
Pat Howell <patrick.howell@outlook.com>
2016-06-29 21:58:12 +0100

    Added controllers

---
+++ WebLauncher/Controllers/TracksController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntityFrameworkAndKnockout.Controllers
+{
+    public class TracksController : Controller
+    {
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
--- WebLauncher/Global.asax.cs
+++ WebLauncher/Global.asax.cs
@@ -14,12 +14,12 @@ namespace EntityFrameworkAndKnockout
     {
         protected void Application_Start()
         {
-            using (var context = new Context())
-            {
-                var d = new Driver { Name = "Bob" };
-                context.Drivers.Add(d);
-                context.SaveChanges();
-            }
+            //using (var context = new Context())
+            //{
+            //    var d = new Driver { Name = "Bob" };
+            //    context.Drivers.Add(d);
+            //    context.SaveChanges();
+            //}
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
