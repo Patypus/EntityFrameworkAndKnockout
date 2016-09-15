4744fe5fb67201ef3bf938237c3ae3c59aab3651
patrick howell <patrick.howell@outlook.com>
2016-09-15 21:41:41 +0100

    Added slide binder

---
+++ WebLauncher/Scripts/binders/slide-down-binder.js
@@ -0,0 +1,14 @@
+﻿ko.bindingHandlers.slidedownbinder = {
+    init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+        $(element).hide();
+    },
+    update: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+        var displayValue = ko.unwrap(valueAccessor());
+
+        if (displayValue) {
+            $(element).slideDown();
+        } else {
+            $(element).slideUp();
+        }
+    }
+}
