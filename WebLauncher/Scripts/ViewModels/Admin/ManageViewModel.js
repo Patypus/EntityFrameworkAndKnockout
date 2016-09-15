46f099c3a92f55f6917f4eb1a94fcbe933eca89f
patrick howell <patrick.howell@outlook.com>
2016-09-15 21:56:12 +0100

    Adding usage of slide binders

--- WebLauncher/Scripts/ViewModels/Admin/ManageViewModel.js
+++ WebLauncher/Scripts/ViewModels/Admin/ManageViewModel.js
@@ -1,8 +1,27 @@
 ﻿var manageViewModel = function (data) {
     var self = this;
-    debugger;
+    
     self.createableYears = ko.observableArray(data.YearsWithNoCreatedSeasons);
     self.editableYears = ko.observableArray(data.EditableSeasons);
+
+    self.showViewForm = ko.observable(false);
+    self.showEditForm = ko.observable(false);
+    self.showAddForm = ko.observable(false);
+
+    self.displayViewButton = function () {
+        var currentDisplayValue = self.showViewForm();
+        self.showViewForm(!currentDisplayValue);
+    }
+
+    self.displayEditButton = function () {
+        var currentDisplayValue = self.showEditForm();
+        self.showEditForm(!currentDisplayValue);
+    }
+
+    self.displayAddButton = function () {
+        var currentDisplayValue = self.showAddForm();
+        self.showAddForm(!currentDisplayValue);
+    }
 
     self.addYear = function () {
         debugger;
